c785c9fd200fec8e920c612f061eb1a81976e4ab
yeony1011 <yeony1011@gmail.com>
2024-12-18 19:50:27 +0900

    update : section02 > chapter02

--- section02/chapter02.js
+++ section02/chapter02.js
@@ -20,7 +20,7 @@ function returnTure(){
   return true;
 }
 
-// console.log(returnFalse() && returnTure()); // False함수, false (단락평가 작동)
+console.log(returnFalse() && returnTure()); // False함수, false (단락평가 작동)
 console.log(returnTure() && returnFalse()); // True함수, False함수, false (단락평가 미작동)
 
 console.log(returnTure() || returnFalse()); // True 함수, true (단락평가 작동)
@@ -28,20 +28,22 @@ console.log(returnFalse() || returnTrue()); // False함수, True 함수, true (�
 
 
 
-function returnFalse(){
+function returnFalse2(){
   console.log("False 함수");
   return undefined;
 }
-function returnTure(){
+function returnTure2(){
   console.log("True 함수");
   return 10;
 }
-console.log(returnTure() || returnFalse()); // True 함수, 10 ( Truthy한 값으로, ture가 아닌 Truthy한 값을 노출함 )
-console.log(returnFalse() && returnTure()); // False 함수, undefined
+console.log(returnTure2() || returnFalse2()); // True 함수, 10 ( Truthy한 값으로, ture가 아닌 Truthy한 값을 노출함 )
+console.log(returnFalse2() && returnTure2()); // False 함수, undefined
 
 
 
 // 단락 평가 활용 사례 (⭐️)
+// gpt 설명 : https://chatgpt.com/share/6762a8ba-c7ac-8003-8556-5c0a6ed944ee
+
 function printName(person){
   const name = person && person.name;
   console.log(name || 'person의 값이 없음');
@@ -49,5 +51,14 @@ function printName(person){
 printName(); // person의 값이 없음
 printName({name: "조승연"}); // 조승연
 
+
+// [⭐️ 정리]
 // T || T -> 첫번째 T값 노출
 // T && T -> 두번째 T값 노출
+
+// && 연산자 : 좌항이 truthy 일때만 우향을 평가
+// || 연산자 : 좌항이 falsy 일때만 우향을 평가
+
+// 단락평가를 활용하는 이유
+// - 안전한 객체 접근 : 객체가 undefined일 수 있는 상황에서 속성을 안전하게 접근
+// - 기본값 처리 : 값이 없는 경우 기본값을 설정할 때 유용
