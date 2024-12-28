e100672345d550eb23b3c08ea0926401c93731c3
yeony1011 <yeony1011@gmail.com>
2024-12-28 22:55:44 +0900

    add : section03 > chapter01, chapter02, chapter03

---
+++ section03/chapter01.js
@@ -0,0 +1,6 @@
+// node.js
+// web 브라우저가 아닌 환경에서도, 자바스크립트 실행 환경 (= 구동기)
+// 실행환경 = Run Time
+// node 사이트의 소개 : Node.js = 크롬 V8 javascript 엔진으로 빌드된 javascript 런타임
+// 노드 = javascipt를 웹환경 밖에서 사용할 수 있게 도와줌
+// 웹서버, 모바일 앱, 데스크톱 앱, 웹 등등 쓰임
---
+++ section03/chapter03.js
@@ -0,0 +1,13 @@
+// 노드에서는 목적을 갖는 프로젝트 == 패키지
+
+// 패키지 만들기 & 초기화
+// npm init
+// -> package.json 파일이 만들어짐
+
+// index.js을 노드로 실행시키기
+// node index.js(파일, 경로)
+// ex) node index1.js, node src/index.js
+
+// node 명령을 계속 하기에 복잡해 질 수 있어서, 명령어를 매크로처럼 만들 수 있음
+// package.json에서 sctipts 안에서 만듦
+// npm run start(만든 명령어)
