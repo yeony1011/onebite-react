62619047df5244b91fc2a7502f6ba48a3750d89d
yeony1011 <yeony1011@gmail.com>
2025-01-04 15:02:11 +0900

    add : section04 > 02. React App 구동원리 살펴보기

--- section04/src/main.jsx
+++ section04/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// createRoot함수를 통해 인수에 document.getElementById('root')를 넣음
+// createRoot 요소를 리액트의 루트로 변환해주고 있음
+// App 컴포넌트를 렌더링하는 중
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
