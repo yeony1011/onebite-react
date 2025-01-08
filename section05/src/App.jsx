de749e2ed004bd3bf5a4edbea5da21efa630234d
yeony1011 <yeony1011@gmail.com>
2025-01-08 20:15:58 +0900

    add : section05 > 06. State로 상태관리하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,47 +1,47 @@
 import './App.css'
-import Header from './components/Header' // 리액트앱을 개발할때, 파일의 확장자 생략 가능!
-import Main from './components/Main'
-import Footer from './components/Footer'
-import Button from './components/Button'
+import { useState } from 'react' // 리액트가 제공하는 내장 함수 사용
 
-function App() { // 부모 컴포넌트 // Root 컴포넌트로 조상으로 있어야함
+function App() {
+  // const state = useState(); // useState()함수를 호출해서 state변수에 저장
+  // console.log(state); // [undefined, 함수] state의 현재값, state의 상태변화 시키는 함수 두개의 값이 들어간 배열이 출력됨
+  
+  // const [state, setState] = useState(0); // 비구조화할당(=구조분해할당)
+  
+  // return (
+  //   <>
+  //     <h1>{state}</h1>
+  //     <button onClick={()=>{
+  //       setState(state + 1)
+  //     }}>+</button>
+  //   </>
+  // )
 
-  const buttonProps = {
-    text: '메일',
-    color: 'red',
-    a: 1,
-    b: 2,
-    c: 3
-  };
-
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState('OFF');
+  
   return (
     <>
-      {/* <Header />
-      <Main />
-      <Footer /> */}
-
-      {/* <Button text={'메일'} color={'red'} /> */}
-      
-      {/* 넘겨야 하는 props가 많은경우, 스프레드 연산자 사용 */}
-      <Button {...buttonProps} /> 
-      
-      {/* props는 부모컴포넌트에서 자식컴포넌트로만 전달 가능 */}
-      <Button text={'카페'} />
-      
-      {/* props는 javascript값 뿐만 아니라, HTML이나 React컴포넌트도 전달할 수 있음(children props로 전달됨) */}
-      <Button text={'블로그'}>
-        <div>자식요소</div>
-      </Button>
-      <Button text={'블로그'}>
-        <Header />
-      </Button>
-
-
-      {/* <Footer text={'푸터'} /> */}
-      {/* props(Properties) : 컴포넌트에 전달된 값들 */}
-      {/* <h1>안녕 !</h1> */}
+    <div>
+      <h1>{light}</h1>
+      <button onClick={()=> {
+        setLight(light === 'OFF' ? 'ON' : 'OFF')
+      }}>전구 {light == 'OFF' ? '켜기' : '끄기'}</button>
+    </div>
+      <div>
+        <h1>{count}</h1>
+        <button onClick={()=>{
+          setCount(count + 1)
+        }}>+</button>
+      </div>
     </>
   )
 }
 
 export default App
+
+
+// state : 컴포넌트의 현재 상태를 보관하는 변수
+// 컴포넌트들은 state 값에 따라 현재 UI 화면을 렌더링
+// 여러개의 state 만드는 것 가능
+
+// 컴포넌트의 값이 변화 -> return문을 다시 렌더링 => 리렌더링
