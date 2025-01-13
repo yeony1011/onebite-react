52e985744d1c12147b858685f88c2ccbebedace4
yeony1011 <yeony1011@gmail.com>
2025-01-13 17:04:44 +0900

    add : section05 > 09. State로 사용자 입력 관리하기, 10.useRef로 컴포넌트의 변수 생성하기

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -1,5 +1,4 @@
-import { use } from 'react';
-import {useState} from 'react';
+import {useState, useRef} from 'react';
 
 // 간단한 회원가입 폼
 // 수집 목록
@@ -9,42 +8,88 @@ import {useState} from 'react';
 // 4. 자기소개
 const Register = () => {
 
-  const [name, setName] = useState('이름');
-  const [birth, setBrith] = useState('');
-  const [country, setCountry] = useState('');
-  const [bio, setBio] = useState('');
+  // const [name, setName] = useState('이름');
+  // const [birth, setBrith] = useState('');
+  // const [country, setCountry] = useState('');
+  // const [bio, setBio] = useState('');
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: ""
+  });
+  // console.log(input);
+
+  const countRef = useRef(0);
+  const inputRef = useRef();
+
+  const onChange = (e) => {
+    console.log(e.target.name, e.target.value);
+    countRef.current++;
+    console.log(countRef.current);
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const onSubmit = () => {
+    if(input.name === ""){
+      inputRef.current.focus();
+    }
   }
 
-  const onChangeBirth = (e) => {
-    setBrith(e.target.value);
-  }
+  // const onChangeName = (e) => {
+  //   // setName(e.target.value);
+  //   setInput({
+  //     ...input,
+  //     name: e.target.value
+  //   });
+  // }
 
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value);
-  }
+  // const onChangeBirth = (e) => {
+  //   // setBrith(e.target.value);
+  //   setInput({
+  //     ...input,
+  //     birth: e.target.value
+  //   });
+  // }
 
-  const onChangeBio = (e) => {
-    setBio(e.target.value);
-  }
+  // const onChangeCountry = (e) => {
+  //   // setCountry(e.target.value);
+  //   setInput({
+  //     ...input,
+  //     country: e.target.value
+  //   });
+  // }
+
+  // const onChangeBio = (e) => {
+  //   // setBio(e.target.value);
+  //   setInput({
+  //     ...input,
+  //     bio: e.target.value
+  //   });
+  // }
 
   return (
     <div>
-      <div><input value={name} onChange={onChangeName} placeholder={"이름"} /></div>
-      <div><input type="date" value={birth} onChange={onChangeBirth} /></div>
+      <div><input ref={inputRef} name="name" value={input.name} onChange={onChange} placeholder={"이름"} /></div>
+      <div><input name="date" type="date" value={input.birth} onChange={onChange} /></div>
       <div>
-        <select value={country} onChange={onChangeCountry}>
+        <select name="country" value={input.country} onChange={onChange}>
           <option value=""></option>
           <option value="kr">한국</option>
           <option value="us">미국</option>
           <option value="uk">영국</option>
         </select>
-        {country}
+        {input.country}
       </div>
       <div>
-        <textarea value={bio} onChange={onChangeBio} />
+        <textarea name="bio" value={input.bio} onChange={onChange} />
+      </div>
+      <div>
+        <button onClick={onSubmit}>제출</button>
       </div>
     </div>
   );
