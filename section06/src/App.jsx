92dcedf520d8ba90f4d95f3897c68a2c87481ba3
yeony1011 <yeony1011@gmail.com>
2025-01-14 16:36:44 +0900

    add : section06 > 02. UI 구현하기

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -1,12 +1,19 @@
-
 import './App.css'
+import Viewer from './components/Viewer';
+import Controller from './components/Controller';
 
 function App() {
   
   return (
-    <>
-      카운터 앱
-    </>
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer />
+      </section>
+      <section>
+        <Controller />
+      </section>
+    </div>
   )
 }
 
