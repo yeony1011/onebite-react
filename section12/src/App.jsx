9695fe457acf7b0e44b3999a7fe7375d4e5e0d26
yeony1011 <yeony1011@gmail.com>
2025-02-24 16:44:23 +0900

    add : section12 > 12.6 폰트, 이미지, 레이아웃 설정하기

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Diary from './pages/Diary'
 import New from './pages/New'
 import Notfound from './pages/Notfound'
+import { getEmotionImage } from './util/get-emotion-image'
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -16,6 +17,19 @@ function App() {
 
   return (
     <>
+      {/* <div>
+      // public 폴더에 있는 정적 파일을 모아둠
+        <img src={"/emotion1.png"} />
+      </div> */}
+      <div>
+        {/* assets 폴더에 있는 정적 파일을 모아둠 */}
+        {/* 배포에서 dataURI 방식으로 노출됨 */}
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
