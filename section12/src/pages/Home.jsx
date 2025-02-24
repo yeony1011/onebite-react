f131014ec2be5ec51a7ff09f87979f284171b812
yeony1011 <yeony1011@gmail.com>
2025-02-24 15:25:16 +0900

    add : section12 > 12.5 페이지 라우팅 4. 동적 경로

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,4 +1,10 @@
+// import { useSearchParams } from 'react-router-dom';
+
 const Home = () => {
+  // 쿼리스트링 방법
+  // const [params, setParams] = useSearchParams();
+  // console.log(params.get('value'));
+
   return <div>Home</div>;
 }
 
