b5b94b8725f56527c0878aabdde9f047581ca018
yeony1011 <yeony1011@gmail.com>
2025-02-24 17:31:24 +0900

    add : section12 > 12.7 공통 컴포넌트 구현하기

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,41 +1,40 @@
 import './App.css'
-import { Routes, Route, Link, useNavigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Diary from './pages/Diary'
 import New from './pages/New'
 import Notfound from './pages/Notfound'
-import { getEmotionImage } from './util/get-emotion-image'
+import Button from './components/Button'
+import Header from './components/Header'
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
-  const nav = useNavigate();
-  const onClickButton = () => {
-    nav('/new');
-  };
 
   return (
     <>
-      {/* <div>
-      // public 폴더에 있는 정적 파일을 모아둠
-        <img src={"/emotion1.png"} />
-      </div> */}
-      <div>
-        {/* assets 폴더에 있는 정적 파일을 모아둠 */}
-        {/* 배포에서 dataURI 방식으로 노출됨 */}
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Header 
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"Right"} />}
+      />
+
+      <Button text={'123'} onClick={()=>{
+        console.log('123번 버튼 클릭');
+        
+      }} />
+
+      <Button text={'123'} type={'pasitive'} onClick={()=>{
+        console.log('123번 버튼 클릭');
+        
+      }} />
+
+      <Button text={'123'} type={'nagative'} onClick={()=>{
+        console.log('123번 버튼 클릭');
+        
+      }} />
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
