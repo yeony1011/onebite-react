3599ec6c64ca4dd2639b3c51dd6cc93601ac95b9
yeony1011 <yeony1011@gmail.com>
2025-02-26 18:04:27 +0900

    add : section12 > 12.11 Home 페이지 구현하기 2. 기능

--- section12/src/components/DiaryList.jsx
+++ section12/src/components/DiaryList.jsx
@@ -1,19 +1,43 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "./DiaryList.css";
 import Button from "./Button";
 import DiaryItem from "./DiaryItem";
 
-const DiaryList = () => {
+const DiaryList = ({ data }) => {
+  const nav = useNavigate();
+  const [sortType, setSortType] = useState("latest");
+
+  const onChangeSortType = (e) => {
+    setSortType(e.target.value);
+  }
+
+  const getSortedData = () => {
+    return data.toSorted((a, b)=>{
+      if(sortType === "oldest"){
+        return Number(a.createdDate) - Number(b.createdDate);
+      }else{
+        return Number(b.createdDate) - Number(a.createdDate);
+      }
+    });
+    
+    // sort : 어떠한 값도 반환하지 않고 원본배열을 정렬함
+    // toSorted : 원본배열은 그대로 두고 새로운 배열을 반환함
+  }
+
+  const sortedData = getSortedData();
+
   return (
     <div className="DiaryList">
       <div className="menu_bar">
-        <select>
+        <select onChange={onChangeSortType}>
           <option value={"latest"}>최신순</option>
           <option value={"oldest"}>오래된 순</option>
         </select>
-        <Button text={"새 일기 쓰기"} type={"POSITIVE"} />
+        <Button onClick={() => nav("/new")} text={"새 일기 쓰기"} type={"POSITIVE"} />
       </div>
       <div className="list_wrapper">
-        <DiaryItem />
+        {sortedData.map((item) => <DiaryItem key={item.id} {...item} />)}
       </div>
     </div>
   );
--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,16 +1,55 @@
+import { useState, useContext } from "react";
+import { DiaryStateContext } from "../App";
+
 import Header from "../components/Header";
 import Button from "../components/Button";
 import DiaryList from "../components/DiaryList";
 
+const getMonthlyData = (pivotDate, data) => {
+  const beginTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth(),
+    1,
+    0,
+    0,
+    0,
+  ).getTime(); // 1일에 0시 0분 0초에 해당하는 데이트 객체를 만들어서 저장되어 있음
+  const endTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth() + 1,
+    0,
+    23,
+    59,
+    59
+  ).getTime(); // 다음달의 0일로 설정하면 이번달의 마지막날로 설정됨
+  
+  return data.filter(
+    (item) => beginTime <= item.createdDate && item.createdDate <= endTime
+  );
+}
+
 const Home = () => {
+  const data = useContext(DiaryStateContext);
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  const monthlyData = getMonthlyData(pivotDate, data);
+  
+  const onIncreseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  }
+
+  const onDecreseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  }
+
   return (
     <div>
       <Header
-        title={"2024년 2월"}
-        leftChild={<Button text={"<"} />}
-        rightChild={<Button text={">"} />}
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button onClick={onDecreseMonth} text={"<"} />}
+        rightChild={<Button onClick={onIncreseMonth} text={">"} />}
       />
-      <DiaryList />
+      <DiaryList data={monthlyData} />
     </div>
   );
 };
